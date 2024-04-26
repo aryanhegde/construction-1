3955b123668ef7628478f1dcb3ed1abe4b188af3
aryanhegde <hegdearyan000@gmail.com>
2024-04-26 08:19:45 +0530

    CTA

--- components/CTA.jsx
+++ components/CTA.jsx
@@ -1,9 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const CTA = () => {
   return (
-    <section>
-      <div className="flex flex-col items-center justify-center mx-auto  w-[85%]  my-24"></div>
+    <section className="bg-[#432e70]">
+      <div className="flex flex-col w-[85%] md:ml-10 p-14 lg:p-24 text-center md:text-start mx-auto md:mx-0">
+        <h1 className="uppercase font-light text-5xl text-gray-300">
+          Let's build your <br />{" "}
+          <span className="font-semibold">dream home.</span>
+        </h1>
+        <div className="mt-10">
+          <Link
+            href="/contact"
+            className="text-slate-800 font-semibold bg-white py-2 px-8"
+          >
+            CONTACT US
+          </Link>
+        </div>
+      </div>
     </section>
   );
 };
